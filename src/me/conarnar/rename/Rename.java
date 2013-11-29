32f14d111978a353ed93c65889cf0cf511720f9c
Conan Truong <truong.conan@gmail.com>
2013-11-29 15:55:04 -0800

    Plugin template

---
+++ src/me/conarnar/rename/Rename.java
@@ -0,0 +1,10 @@
+package me.conarnar.rename;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Rename extends JavaPlugin implements Listener {
+	public void onEnable() {
+		
+	}
+}
