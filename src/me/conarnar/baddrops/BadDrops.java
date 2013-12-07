b5b035992bb312b4b320096bee6c5086eef27e74
Conan Truong <truong.conan@gmail.com>
2013-12-07 09:11:30 -0800

    Documentation and other stuff

--- src/me/conarnar/baddrops/BadDrops.java
+++ src/me/conarnar/baddrops/BadDrops.java
@@ -1,9 +1,40 @@
 package me.conarnar.baddrops;
 
+import java.util.Random;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BadDrops extends JavaPlugin {
+	private Random rand = new Random();
+	public double odds;
+	
 	public void onEnable() {
+		// registering listener
+		getServer().getPluginManager().registerEvents(new BadDropsListener(this), this);
 		
+		// getting config
+		odds = getConfig().getDouble("odds", 0.06);
+		
+		// checking if the value given for the config is valid
+		if (odds < 0) odds = 0;
+		else if (odds > 1) odds = 1;
+		
+		// saving to config the value of odds
+		// if it wasn't given, or there was no config file, this will create one
+		getConfig().set("odds", odds);
+	}
+	
+	/**
+	 * Random number generator to determine whether something will happen.
+	 * @param odds - The chance that this will return true.
+	 * @return Whether the randomly generated number is less than "odds".
+	 * @throws IllegalArgumentException If odds is not between 0 and 1.
+	 */
+	public boolean chance(double odds) {
+		if (odds < 0 || odds > 1) {
+			throw new IllegalArgumentException("odds must be between 0 and 1");
+		}
+		
+		return rand.nextDouble() < odds;
 	}
 }
--- src/me/conarnar/baddrops/BadDropsListener.java
+++ src/me/conarnar/baddrops/BadDropsListener.java
@@ -1,11 +1,24 @@
 package me.conarnar.baddrops;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class BadDropsListener implements Listener {
 	private final BadDrops plugin;
 	
 	public BadDropsListener(BadDrops drops) {
 		plugin = drops;
+	}
+	
+	/**
+	 * Listens for EntityDeathEvent to know every time a mob dies.
+	 * @param evt - The event to listen for.
+	 */
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent evt) {
+		if (plugin.chance(plugin.odds)) {
+			
+		}
 	}
 }
